f22d5d3904594b52c710d24a866f1019ec593a32
Jarred <jarredjay03@gmail.com>
2026-09-21 03:35:45 -0400

    Added a sample unit test to show placement for all future test classes.

---
+++ src/test/java/com/spotanote/AppTest.java
@@ -0,0 +1,13 @@
+package com.spotanote;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    void sampleTest(){
+        int result = 2 + 2;
+        assertEquals(4, result, "Sample Unit Test");
+    }
+}
