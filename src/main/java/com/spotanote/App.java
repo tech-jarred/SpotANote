c7e6cc8c5680e108851cbd1037b4fb8ceddb2640
HazelPatel1 <hazelpatel2005@gmail.com>
2026-09-23 14:03:22 -0400

    MusicUpload.java + database connections
    Created the foundations for database connection within MusicUpload.java. Also created MusicUploadTest.java as an initial starting place for testing. App.java was updated to include an instance of MusicUpload for testing database connection.

--- src/main/java/com/spotanote/App.java
+++ src/main/java/com/spotanote/App.java
@@ -4,5 +4,8 @@ public class App {
     public static void main(String[] args){
         // just a placeholder
         System.out.println("Hello world!");
+
+        //basic testing to see if database connections work for MusicUpload.Java
+        MusicUpload.musicUpload("Testing", 1, "Testing", 1);
     }
 }
