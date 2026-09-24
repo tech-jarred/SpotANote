66a5f7bb132846839c0938f43006ce191d9c5f76
Connor Singer <connor.singer@icloud.com>
2026-09-24 11:42:51 -0400

    Added tests to createPromotions

---
+++ src/main/java/Promotion.java
@@ -0,0 +1,40 @@
+//import java.util.Objects;
+package com.spotanote;
+
+public class Promotion {
+
+    private final int id;
+    private String promotionName;
+    private String promotionDescription;
+    private String promotionSong;
+    private String recordName;
+
+
+    public Promotion(int id, String promotionName, String promotionDescription, String promotionSong, String recordName){
+        this.id = id;
+        this.promotionName = promotionName;
+        this.promotionDescription = promotionDescription;
+        this.promotionSong = promotionSong;
+        this.recordName = recordName;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getPromotionName(){
+        return promotionName;
+    }
+
+    public String getPromotionDescription(){
+        return promotionDescription;
+    }
+
+    public String getPromotionSong(){
+        return promotionSong;
+    }
+
+    public String getRecordName(){
+        return recordName;
+    }
+}
