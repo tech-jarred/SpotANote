3baa262e6a1088b689206badd5721ff74677a47f
HazelPatel1 <hazelpatel2005@gmail.com>
2026-09-24 20:19:19 -0400

    Branch Additions
    Basic java and junit files created. Minor Update to sql.
    Majority of code written in PrivatePlaylist.java

---
+++ src/test/java/com/spotanote/PrivatePlaylistTest.java
@@ -0,0 +1,59 @@
+/*
+ * PrivatePlaylistTest class
+ * Focuses on testing the PrivatePlaylist class
+ */
+
+//imports
+package com.spotanote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class PrivatePlaylistTest {
+    
+    /*
+     * sets up environment for testing before each test is ran
+     */
+    @BeforeEach
+    void setUp()
+    {
+        
+    }
+
+    /*
+     * Tests to see if the playlist has been privated successfully
+     */
+    @Test
+    void successfullPrivatePlaylistTest()
+    {
+        //
+    }
+
+    /*
+     * Tests to see what happens when the playlist wasn't actually privated
+     */
+    @Test
+    void unsuccessfulPrivatePlaylistTest()
+    {
+        //
+    }
+
+    /*
+     * Tests to see what happens when the incorrect playlist is privated
+    */
+    @Test
+    void incorrectPlaylistPrivatedTest()
+    {
+        //
+    }
+
+    /*
+     * test to see what happens when trying to private an already private playlist
+    */
+    @Test
+    void alreadyPrivatePlaylistTest()
+    {
+        //
+    }
+
+}
